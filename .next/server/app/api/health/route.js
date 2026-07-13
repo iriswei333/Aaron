4cc8c1c5ddfb875de27a3718eb00625a026415e4
Iris Wei <iriswei@mac.lan>
2026-07-13 15:51:52 -0700

    add background support

--- .next/server/app/api/health/route.js
+++ .next/server/app/api/health/route.js
@@ -65,7 +65,7 @@ module.exports = require("next/dist/server/app-render/work-async-storage.externa
 
 /***/ }),
 
-/***/ "../app-render/work-unit-async-storage.external":
+/***/ "./work-unit-async-storage.external":
 /*!**********************************************************************************!*\
   !*** external "next/dist/server/app-render/work-unit-async-storage.external.js" ***!
   \**********************************************************************************/
